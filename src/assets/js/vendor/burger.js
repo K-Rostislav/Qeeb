ce12259c559e1fbdc0a3d6200e91d14ebc103b18
K-Rostislav <moregan123456789@gmail.com>
2023-03-17 17:51:35 +0300

    commit

--- src/assets/js/vendor/burger.js
+++ src/assets/js/vendor/burger.js
@@ -1,17 +1,24 @@
-const burgerBtn = document.querySelector('.burger-btn');
-const burger = document.querySelector('.burger');
+const burgerBtn = document.querySelector('.burger-btn'),
+      burgerBg = document.querySelector('.burger-background'),
+      burger = document.querySelector('.burger')
+      close = document.querySelector('.close')
+      body = document.body
 
-burgerBtn.addEventListener('click', (e) => {
-    burgerBtn.classList.toggle('burger-btn_active');
-    burger.classList.toggle('burger_active');
-    document.body.classList.toggle('body_no-scroll');
-    e.stopPropagation()
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.add('burger-btn_active')
+    burger.classList.add('burger_active')
+    burgerBg.classList.add('burger-background_active')
+    body.classList.add('body_no-scroll')
 })
-
-window.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('burger') && !e.target.classList.contains('header__list')) {
-        burgerBtn.classList.remove('burger-btn_active');
-        burger.classList.remove('burger_active');
-        document.body.classList.remove('body_no-scroll');
-    }
+burgerBg.addEventListener('click', () => {
+    burgerBtn.classList.remove('burger-btn_active')
+    burger.classList.remove('burger_active')
+    burgerBg.classList.remove('burger-background_active')
+    body.classList.remove('body_no-scroll')
+})
+close.addEventListener('click', () => {
+    burgerBtn.classList.remove('burger-btn_active')
+    burger.classList.remove('burger_active')
+    burgerBg.classList.remove('burger-background_active')
+    body.classList.remove('body_no-scroll')
 })
